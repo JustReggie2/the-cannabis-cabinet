4aa2f848713c24b84e6123a957b72336b18ce547
Reginald T Carter iI <reginaldcarterii@gmail.com>
2019-01-20 21:09:44 +0000

    add create_cabinet_strains table

---
+++ app/models/cabinet_strain.rb
@@ -0,0 +1,3 @@
+class CabinetStrain < ActiveRecord::Base
+
+end
---
+++ db/migrate/20190120210612_create_cabinet_strains.rb
@@ -0,0 +1,8 @@
+class CreateCabinetStrains < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cabinet_strains do |t|
+      t.integer :cabinet_id
+      t.integer :strain_id
+    end
+  end
+end
