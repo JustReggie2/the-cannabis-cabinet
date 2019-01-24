1b7b0acbee2a7deefdf5ab1431759561b469f2a5
Reginald T Carter iI <reginaldcarterii@gmail.com>
2019-01-24 05:02:10 +0000

    commented out extend module for cabinet.rb

--- app/models/cabinet.rb
+++ app/models/cabinet.rb
@@ -1,6 +1,6 @@
 class Cabinet < ActiveRecord::Base
-  extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
+  # extend Slugifiable::ClassMethods
+  # include Slugifiable::InstanceMethods
   belongs_to :user
   has_many :cabinet_strains
   has_many :strains, through: :cabinet_strains
