d061fb2b6794fd83d03af66ab857e8a13bf1d1d9
Reginald T Carter iI <reginaldcarterii@gmail.com>
2019-01-24 04:02:19 +0000

    add seeds.rb and started strains list

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+strains_list = {
+  "Bugatti OG" => {
+    category: "Hybrid",
+    positives: "Relaxing and good for pain.",
+    negatives: "Dry eyes and dry mouth."
+  },
+  "White Fire 43" => {
+    category: "Indica",
+    positives: "Relaxing and good for insomnia.",
+    negatives: "Dry eyes."
+  },
+  "Candyland" => {
+    category: "Sativa",
+    positives: "Happy and good for depression.",
+    negatives: "Dry mouth"
+  },
+  "Pink Lemonade" => {
+    category: "Hybrid",
+    positives: "Happy and good for stress.",
+    negatives: "Dry mouth and can become anxious"
+  },
+}
