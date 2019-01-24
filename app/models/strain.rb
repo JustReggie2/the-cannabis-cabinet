5c9a8fa00a7e2bf98610c01b5fff79b0428fe46b
Reginald T Carter iI <reginaldcarterii@gmail.com>
2019-01-24 03:23:41 +0000

    add extend/include slug to models

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -1,4 +1,6 @@
 class Strain < ActiveRecord::Base
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
   has_many :cabinet_strains
   has_many :cabinets, through: :cabinet_strains
   has_many :users, through: :cabinets
