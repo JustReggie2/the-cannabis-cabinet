5df91fd611c6297cc322e6b66068a1ab7c5533c0
Reginald T Carter iI <reginaldcarterii@gmail.com>
2019-01-24 18:05:29 +0000

    add get /mycabinets/new

--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -1,3 +1,10 @@
 class CabinetsController < ApplicationController
 
+  get '/mycabinets/new' do
+    if Helpers.logged_in?(session)
+      erb :'/cabinets/new'
+    else
+      redirect 'users/login'
+    end
+  end
 end
