fca92baf66212b53019bc85fccc6bf5d34b714ab
Reginald T Carter iI <reginaldcarterii@gmail.com>
2019-01-27 23:32:45 +0000

    add get /strains/new

--- app/controllers/strains_controller.rb
+++ app/controllers/strains_controller.rb
@@ -6,6 +6,14 @@ class StrainsController < ApplicationController
     erb :'strains/index'
   end
 
+  get '/strains/new' do
+    if Helpers.logged_in?(session)
+      erb :'/strains/new'
+    else
+      redirect 'users/login'
+    end
+  end
+
   get '/strains/:slug' do
     @strain = Strain.find_by_slug(params[:slug])
 
